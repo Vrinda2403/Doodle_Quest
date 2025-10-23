0039a0f92ae9a80aaf02fa52004f6396655c1803
Prabhleen-11 <prabhukaur011@gmail.com>
2025-10-23 18:54:51 +0530

    Image for story

--- Server_Side/controllers/storycontroller.js
+++ Server_Side/controllers/storycontroller.js
@@ -1,9 +1,11 @@
+import storyimage from "../services/storyimage.js";
 import storyContent from "../services/storyservice.js";    
 
 
 async function generateStory(req, res)
 {
 const story=await storyContent(req.query.obj)
+await storyimage(story);
 res.status(200).json({ story });
 }
 export default generateStory;
