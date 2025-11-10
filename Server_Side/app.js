81c2e598ab0bd1813362b30d9732e21ba33e114b
Prabhleen-11 <prabhukaur011@gmail.com>
2025-11-10 22:56:11 +0530

    Audio Section Started

--- Server_Side/app.js
+++ Server_Side/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 
 import storyroute from "./routes/storyroute.js";
 import quizroute from "./routes/quizroute.js";
+import audioService from "./services/audioservice.js";  
 const app = express();
 app.use(cors());
 
@@ -10,4 +11,10 @@ app.use(express.json());
 
 app.use("/api",storyroute);
 app.use("/api",quizroute);
+const story=`After the rain stopped, Peppa and George ran outside to jump in muddy puddles.
+Suddenly, they saw a big, bright rainbow in the sky!
+
+“Let’s find the end of the rainbow!” giggled Peppa.
+But Daddy Pig chuckled — “The rainbow’s end is wherever the fun is!`
+audioService(story);
 export default app;
